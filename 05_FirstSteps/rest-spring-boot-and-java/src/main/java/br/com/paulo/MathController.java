477865a485cd572572240bb6c517820f03a36c66
Paulo <p.dantas@outlook.com>
2023-12-07 18:21:13 -0300

    sum method with validation

---
+++ 05_FirstSteps/rest-spring-boot-and-java/src/main/java/br/com/paulo/MathController.java
@@ -0,0 +1,33 @@
+package br.com.paulo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
+			method = RequestMethod.GET)
+	public Double sum (
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo
+			) throws Exception {
+		
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+	
+	private boolean isNumeric(String strNumber) {
+		return false;
+	}
+	private Double convertToDouble(String strNumber) {
+		return null;
+	}
+}

a3822e1d64f71bb6fbc6da0ab5ee99c5577b0e26
Paulo <p.dantas@outlook.com>
2024-01-09 16:49:30 -0300

    Person model, services and controller

---
+++ 05_FirstSteps/rest-spring-boot-and-java/src/main/java/br/com/paulo/controllers/PersonController.java
@@ -0,0 +1,35 @@
+package br.com.paulo.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.paulo.models.Person;
+import br.com.paulo.services.PersonServices;
+
+
+@RestController
+@RequestMapping(value = "/person")
+public class PersonController {
+	
+	@Autowired
+	private PersonServices service;
+	//private PersonServices = new PersonServices();
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	
+	@RequestMapping(value = "/{id}",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById (@PathVariable (value = "id") String id) throws Exception{
+		
+		return service.findById(id);	
+	}
+	
+}

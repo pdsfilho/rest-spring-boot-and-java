9c7c71bd62859bb577f9eb35920f1f8d2bedb544
Paulo <p.dantas@outlook.com>
2024-01-24 20:21:02 -0300

    Annotations and yml changes

--- 05_FirstSteps/rest-spring-boot-and-java/src/main/java/br/com/paulo/controllers/PersonController.java
+++ 05_FirstSteps/rest-spring-boot-and-java/src/main/java/br/com/paulo/controllers/PersonController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -60,8 +61,9 @@ public class PersonController {
 
 	
 	@DeleteMapping(value = "/{id}")
-	public void delete (@PathVariable (value = "id") Long id){
+	public ResponseEntity<?> delete (@PathVariable (value = "id") Long id){
 		
 		service.delete(id);	
+		return ResponseEntity.noContent().build();
 	}
 }

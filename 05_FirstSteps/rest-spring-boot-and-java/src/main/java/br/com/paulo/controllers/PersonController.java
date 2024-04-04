d80edfb7122664cb547ace28bdb27802357926fd
Paulo <p.dantas@outlook.com>
2024-04-04 16:21:08 -0300

    DozerMapper

--- 05_FirstSteps/rest-spring-boot-and-java/src/main/java/br/com/paulo/controllers/PersonController.java
+++ 05_FirstSteps/rest-spring-boot-and-java/src/main/java/br/com/paulo/controllers/PersonController.java
@@ -1,7 +1,6 @@
 package br.com.paulo.controllers;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,55 +14,43 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.paulo.models.Person;
+import br.com.paulo.data.vo.v1.PersonVO;
 import br.com.paulo.services.PersonServices;
 
-
 @RestController
-@RequestMapping(value = "/person")
+@RequestMapping("/person")
 public class PersonController {
 	
 	@Autowired
 	private PersonServices service;
-	//private PersonServices = new PersonServices();
 	
-	private final AtomicLong counter = new AtomicLong();
-	
-	
-	@GetMapping(
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll(){
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<PersonVO> findAll() {
 		return service.findAll();
-		}
+	}
 	
 	@GetMapping(value = "/{id}",
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById (@PathVariable (value = "id") Long id) throws Exception{
-		
-		return service.findById(id);	
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
+		return service.findById(id);
 	}
 	
-	@PostMapping(
-			consumes = MediaType.APPLICATION_JSON_VALUE,
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create (@RequestBody Person person){
-		
-		return service.create(person);	
+	public PersonVO create(@RequestBody PersonVO person) {
+		return service.create(person);
 	}
 	
-	@PutMapping(
-			consumes = MediaType.APPLICATION_JSON_VALUE,
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update (@RequestBody Person person){
-		
+	public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
 	}
-
+	
 	
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<?> delete (@PathVariable (value = "id") Long id){
-		
-		service.delete(id);	
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 }

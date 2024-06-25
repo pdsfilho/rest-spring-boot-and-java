a59dc381c36c40049c38058dec5934935317b159
Paulo <p.dantas@outlook.com>
2024-06-25 11:43:30 -0300

    Json Properties

--- 05_FirstSteps/rest-spring-boot-and-java/src/main/java/br/com/paulo/data/vo/v1/PersonVO.java
+++ 05_FirstSteps/rest-spring-boot-and-java/src/main/java/br/com/paulo/data/vo/v1/PersonVO.java
@@ -2,13 +2,22 @@ package br.com.paulo.data.vo.v1;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "firstName", "lastName","address","gender"})
 public class PersonVO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
+	
+	@JsonProperty("first_name")
 	private String lastName;
+	
 	private String address;
 	private String gender;
 	

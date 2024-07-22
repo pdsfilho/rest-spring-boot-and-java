82a970a7e121b10c5a593c092d240d57bb496d4d
Paulo <p.dantas@outlook.com>
2024-07-22 18:24:23 -0300

    Adding Swagger

---
+++ 05_FirstSteps/rest-spring-boot-and-java/src/main/java/br/com/paulo/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package br.com.paulo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("RESTful API with Java 17 and Spring Boot 3.2")
+						.version("v1")
+						.description("Some description about your API")
+						.termsOfService("")
+						.license(
+								new License()
+								.name("Apache 2.0")
+								.url("https://github.com/pdsfilho/rest-spring-boot-and-java")
+								)
+						);
+	}
+}
